f828c1e7569692cc6ff56aca73760dc0c51e9348
gaogaotiantian <gaogaotiantian@hotmail.com>
2017-05-04 16:16:38 -0700

    Add multi language

---
+++ language.js
@@ -0,0 +1,16 @@
+var text_multi_language = {
+    "en-us" : {
+        "match_status" : "Match Status",
+        "your_team" : "Your Team",
+        "enemy_team" : "Enemy Team",
+        "win_rate" : "Win Rate",
+        "choose_hero" : "Choose Hero"
+    },
+    "zh-cn" : {
+        "match_status" : "比赛形势",
+        "your_team" : "你的队伍",
+        "enemy_team" : "对方队伍",
+        "win_rate" : "胜率",
+        "choose_hero" : "选择英雄"
+    }
+};
